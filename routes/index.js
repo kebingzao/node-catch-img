ae4f198bb7b47ca4510c6ad2a37d80c5a34d8a2a
kbz <kebingzao@gmail.com>
2015-11-10 17:09:41 +0800

    修改细节

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var airHelper = require('../lib/helper');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var url = "http://item.jd.com/2025639.html";
+  res.render('index', {
+    title: 'Express'
+  });
+});
 
+/* GET catch page. */
+router.get('/catch', function(req, res, next) {
+  var name = req.query["name"];
+  //var url = "http://item.jd.com/2025639.html";
+  var url = req.query["url"];
   airHelper.getPageData(url).then(function(data) {
     airHelper.getAllImg(data, ".spec-items li img").then(function(zipName){
       // 下载到本地
@@ -13,7 +22,6 @@ router.get('/', function(req, res, next) {
   },function(){
     console.log("error");
   });
-  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
