7a4f22a3de50512c9692f1ca9ff299edaac119d8
kbz <kebingzao@gmail.com>
2015-11-10 18:02:46 +0800

    修改细节

--- routes/index.js
+++ routes/index.js
@@ -24,8 +24,6 @@ router.get('/catch', function(req, res, next) {
       airHelper.getPageData(url).then(function(data) {
         // 获取数据并下载
         airHelper.getAllImg(data, ".spec-items li img", fileName).then(function(){
-          // todo http://img14.360buyimg.com/n5/jfs/t2053/317/924464287/25158/b0e589f2/5631d1a9N4668d62a.jpg
-          // 只要把链接中的n5，改成n4，n3，n2，n1，就可以下载对应的图片
           // 接下来是保存
           airHelper.writeZip(fileName + "/",name,function(zipName){
             // 最后下载到本地
