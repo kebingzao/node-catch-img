a7d1fb1ca7370936515a434577bb829225f9f36d
kbz <kebingzao@gmail.com>
2015-11-11 16:03:34 +0800

    修改细节

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var jd = require('./routes/jd');
+var aso = require('./routes/aso');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/jd', jd);
+app.use('/aso', aso);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/aso.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var cheerio = require("cheerio");
+var fs = require("fs");
+var Q = require("q");
+var _ = require("underscore");
+var airHelper = require('../lib/helper');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('aso', {
+    data: ''
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var url = req.body["url"];
+  var pageData = {
+    title: "",
+    time: "",
+    th: ['名次','名字','厂商'],
+    td: []
+  };
+  // 获取dom
+  airHelper.getPageData(url).then(function(data) {
+    // 获取数据并下载
+    var $ = cheerio.load(data);
+    pageData.time = $(".date-range-picker span").html();
+    pageData.title = $("title").html();
+    var tdArr = [];
+    $(".rank-list .thumbnail .caption").each(function(i, e) {
+      var name = $(e).find("h5").html();
+      var num = name.split(".")[0];
+      tdArr.push({
+        // 这边不需要总榜
+        //num: $(e).find("h6 span").html(),
+        num: num,
+        name: name.substr(num.length + 1),
+        company: $($(e).find("h6")[0]).html()
+      })
+    });
+    pageData.td = tdArr;
+    res.render('aso', {
+      data: pageData
+    });
+  },function(){
+    console.log("error");
+  });
+});
+
+module.exports = router;
