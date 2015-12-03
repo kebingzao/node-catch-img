33659c1482d60727f1c32b24e0be1b2da34d6e68
kbz <kebingzao@gmail.com>
2015-12-03 21:06:08 +0800

    抓取一元夺宝

--- routes/duobao.js
+++ routes/duobao.js
@@ -12,5 +12,49 @@ router.get('/', function(req, res, next) {
 
   });
 });
+// 网易的一元夺宝的数据
+router.get('/yiyuan', function(req, res, next) {
+  res.render('duobao/yiyuan', {
+    data: ''
+  });
+});
+
+router.post('/yiyuan', function(req, res, next) {
+  var pageData = {
+    title: "网易一元夺宝",
+    th: ['编号','名称'],
+    td: []
+  };
+  // 最大页数
+  var maxPage = 10;
+  var getPageUrl = function(index){
+    return "http://1.163.com/list/0-0-1-"+ index +".html";
+  };
+  var count = 1;
+  var doCatch = function(){
+    if(count > maxPage){
+      res.render('duobao/yiyuan', {
+        data: pageData
+      });
+    }else{
+      airHelper.getPageData(getPageUrl(count)).then(function(data) {
+        // 获取数据并下载
+        var $ = cheerio.load(data);
+        var tdArr = [];
+        $(".w-quickBuyList-item .w-goods-title a").each(function(i, e) {
+          tdArr.push({
+            name: $(e).html()
+          })
+        });
+        pageData.td = pageData.td.concat(tdArr);
+        count += 1;
+        doCatch();
+      },function(){
+        console.log("error");
+      });
+    }
+  };
+  doCatch();
+});
 
 module.exports = router;
