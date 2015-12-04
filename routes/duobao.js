0029edc4453c64e6998cd45942d759c0b41d3452
kbz <kebingzao@gmail.com>
2015-12-04 18:32:37 +0800

    抓取全民夺宝

--- routes/duobao.js
+++ routes/duobao.js
@@ -57,4 +57,49 @@ router.post('/yiyuan', function(req, res, next) {
   doCatch();
 });
 
+
+// 全民夺宝的数据
+router.get('/quanmin', function(req, res, next) {
+  res.render('duobao/quanmin', {
+    data: ''
+  });
+});
+
+router.post('/quanmin', function(req, res, next) {
+  var pageData = {
+    title: "全民夺宝",
+    th: ['编号','名称'],
+    td: []
+  };
+  // 最大页数
+  var maxPage = 8;
+  var getPageUrl = function(index){
+    return "http://www.qmduobao.com/list/hot20/p_"+ index +".html";
+  };
+  var count = 1;
+  var doCatch = function(){
+    if(count > maxPage){
+      res.render('duobao/quanmin', {
+        data: pageData
+      });
+    }else{
+      airHelper.getPageData(getPageUrl(count)).then(function(data) {
+        // 获取数据并下载
+        var $ = cheerio.load(data);
+        var tdArr = [];
+        $("#ulGoodsList .soon-list-name a").each(function(i, e) {
+          tdArr.push({
+            name: $(e).text().replace(/(\(第\S+期\))(.*)/g, "$2").replace("【全民夺宝】","")
+          })
+        });
+        pageData.td = pageData.td.concat(tdArr);
+        count += 1;
+        doCatch();
+      },function(){
+        console.log("error");
+      });
+    }
+  };
+  doCatch();
+});
 module.exports = router;
