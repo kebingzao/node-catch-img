f53e87f91579143553fe634b094368bc3cdeac0e
kbz <kebingzao@gmail.com>
2015-12-08 10:50:18 +0800

    增加数据的对比

--- routes/duobao.js
+++ routes/duobao.js
@@ -239,10 +239,41 @@ router.get('/past', function(req, res) {
   var doFinish = function(list){
     res.render('duobao/past', {
       title: SITE_LIST[site],
+      site: site,
       list: list || []
     });
   };
   dbSiteHelper.getCollectionAllItem(site,{},{time: -1}).then(doFinish,doFinish);
+});
+
+
+// 两天数据对比
+router.get('/compare', function(req, res) {
+  var site = req.query["site"];
+  var time1 = req.query["time1"];
+  var time2 = req.query["time2"];
+
+  dbSiteHelper.getCollectionItem(site,{time:time1}).then(function(data1){
+    dbSiteHelper.getCollectionItem(site,{time:time2}).then(function(data2){
+      // 接下来开始比较
+      var arr1 = _.map(data1.data.td,function(item){
+        return item.name;
+      });
+      var arr2 = _.map(data2.data.td,function(item){
+        return item.name;
+      });
+      var diff1 = _.difference(arr1, arr2);
+      var diff2 = _.difference(arr2, arr1);
+      res.render('duobao/compare', {
+        title: SITE_LIST[site],
+        targetDif: diff1.length > diff2.length ? diff1 : diff2,
+        diff1: diff1,
+        time1: time1,
+        diff2: diff2,
+        time2: time2
+      });
+    })
+  })
 });
 
 module.exports = router;
