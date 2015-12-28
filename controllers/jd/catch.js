a8ead2dd2580ee210bbcbc958735ef9c4e6db3c0
kbz <kebingzao@gmail.com>
2015-12-28 17:12:34 +0800

    去掉多余的代码

--- controllers/jd/catch.js
+++ controllers/jd/catch.js
@@ -163,20 +163,17 @@ module.exports = function (req, res, next) {
       }
     }
   };
-  // 首先清空tmp目录
-  //airHelper.clearDir(TMPFILE, function(){
-    // 接下来创建文件夹
-    // 接下来创建一个对应文件
-    airHelper.createDir(parentFileName, function(){
-      // 获取dom
-      _.each(urls,function(item){
-        (function(url){
-          doCatchTheImg(url, parentFileName).then(function(){
-            count += 1;
-            doSuccess();
-          })
-        })(item.trim());
-      });
+  // 接下来创建文件夹
+  // 接下来创建一个对应文件
+  airHelper.createDir(parentFileName, function(){
+    // 获取dom
+    _.each(urls,function(item){
+      (function(url){
+        doCatchTheImg(url, parentFileName).then(function(){
+          count += 1;
+          doSuccess();
+        })
+      })(item.trim());
     });
-  //});
+  });
 };
