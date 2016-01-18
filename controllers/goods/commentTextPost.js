db824a120b84ad563e279ee82b9a23b691b02016
kbz <kebingzao@gmail.com>
2016-01-18 11:28:20 +0800

    增加网易晒单文字请求的超时响应

--- controllers/goods/commentTextPost.js
+++ controllers/goods/commentTextPost.js
@@ -1,6 +1,10 @@
 var siteCatch = require('./site');
 // router goods/commentText  post
 module.exports = function (req, res, next) {
+    res.setTimeout(200000, function () {
+        console.log("响应超时.");
+        res.send("页数太多，响应超时");
+    });
     siteCatch["commentWY"](req.body).then(function(data){
         res.render('commentText', {
             data: data
