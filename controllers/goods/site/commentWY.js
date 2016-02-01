19cb6747c39a716d8ee5b7d85a2950f50bf76d08
kbz <kebingzao@gmail.com>
2016-02-01 18:06:27 +0800

    网易抓取评论增加类型筛选

--- controllers/goods/site/commentWY.js
+++ controllers/goods/site/commentWY.js
@@ -1,13 +1,26 @@
 var Q = require("q");
 var _ = require("underscore");
 var airHelper = require('../../../lib/helper');
+var goodsType = {
+  "0":"全部",
+  "1":"手机平板",
+  "2":"电脑办公",
+  "3":"数码影音",
+  "4":"女性时尚",
+  "5":"美食天地",
+  "6":"潮流新品",
+  "7":"网易周边",
+  "8":"其他商品"
+};
 // 抓取全民夺宝数据
 module.exports = function(reqBody){
   var defer = Q.defer();
+  var type = parseInt(reqBody["type"]) || 0;
   var pageData = {
     title: "一元夺宝晒单数据",
     th: ['编号','商品', '标题', '内容'],
-    td: []
+    td: [],
+    type: goodsType[type.toString()]
   };
   var count = parseInt(reqBody["start"]) || 1;
   count < 1 && (count = 1);
@@ -29,12 +42,15 @@ module.exports = function(reqBody){
         data = JSON.parse(data);
         var tdArr = [];
        data.result && data.result.list &&  _.each(data.result.list,function(item){
-         console.log(item.title);
-          tdArr.push({
-            title: item.title,
-            des: item.content,
-            name: item.winRecord.goods.gname
-          })
+         // 判断类型是否对
+         if(type === 0 || (item.winRecord && item.winRecord.goods && item.winRecord.goods.typeId == type)){
+           console.log(item.title);
+           tdArr.push({
+             title: item.title,
+             des: item.content,
+             name: item.winRecord.goods.gname
+           })
+         }
         });
         pageData.td = pageData.td.concat(tdArr);
         count += 1;
